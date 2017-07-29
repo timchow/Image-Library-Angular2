2bbbaefe38915bd98a7d38aed03e1fe24060b4fb
Timothy Chow <timssss@live.com>
2017-07-29 11:01:08 -0500

    ReverseNth is functional - need to clean up!

--- src/app/models/list.ts
+++ src/app/models/list.ts
@@ -24,47 +24,118 @@ export class List {
     public print(reverse: boolean = false): void {
         let output = "",
             cursor = reverse ? this.tail : this.head;
-
-        output = reverse ? this.printBackward(cursor) : this.printForward(cursor);
+        output = reverse ? this._print(cursor,true) : this._print(cursor, false);
         console.log(output);
     }
 
+    private _printList(head: ListNode) {
+        this._print(head);
+    }
+
     // Recursive print
-    private printForward(cursor: ListNode): string {
+    private _print(cursor: ListNode, reverse: boolean = false): string {
         if (cursor == null) {
             return "";
         }
 
-        return cursor.data + " " + this.printForward(cursor.next);
+        let node: ListNode = reverse ? cursor.prev : cursor.next;
+
+        return cursor.data + " " + this._print(node,reverse);
     }
 
-    private printBackward(cursor: ListNode) {
-        if (cursor == null) {
-            return "";
+    public getSize(head: ListNode): number {
+        if (head == null) {
+            return 0;
         }
 
-        return cursor.data + " " + this.printForward(cursor.prev);
+        return 1 + this.getSize(head.next);
     }
 
-    public reverse(): void {
-        if (this.length < 2) { return; }
+    private _reverse(head: ListNode): [ListNode,ListNode] {
+        let length = this.getSize(head);
+        if (length < 1) { return; }
+        else if (length == 1) { return [head,head]; }
  
-        let cursor = this.head,
-            originalhead = this.head;
+        let cursor: ListNode = head, 
+            originalhead: ListNode = head, 
+            cursorShadow: ListNode = head,
+            temp: ListNode, tail: ListNode;
 
-        
         while(cursor != null) {
-            console.log(cursor.data);
-            //this.swapNode(cursor.prev, cursor.next);
+            temp = cursor.prev;
+            cursor.prev = cursor.next;
+            cursor.next = temp;
+
+            cursorShadow = cursor;
             cursor = cursor.prev;
         }
-        //cursor.prev = null;
-        this.head = cursor;
-        this.tail = originalhead;
+        
+        head = cursorShadow;
+        tail = originalhead;
+
+        return [head,tail];
     }
 
-    private swapNode(a: ListNode, b: ListNode): void {
-        [a,b] = [b,a];
+    public reverse() {
+        let temp: [ListNode,ListNode] = this._reverse(this.head);
+        this.head = temp[0];
+        this.tail = temp[1]; 
+        console.log(this.head)
+        console.log(this.tail)
+    }
+
+
+    public reverseNth(n: number): void {
+        if (n < 2) { return }
+        else if (n >= this.length) { this.reverse(); return }
+
+        let idx = 0;
+        let cursor: ListNode = this.head;
+        let originalhead: ListNode = this.head;
+        let prevTail: ListNode;
+        let newHead: ListNode;
+        let start: ListNode;
+        let end: ListNode;
+        let endPoints: [ListNode,ListNode];
+        let nextStart: ListNode;
+        let ITERATIONS = Math.floor(this.length / n);
+        if (this.length % n !=0) {
+            ITERATIONS++;
+        }
+        let j = 0;
+
+        console.log("Iterations: " + ITERATIONS);
+
+        debugger;
+        for(let i = 0; i < ITERATIONS; i++) {
+            newHead = cursor;
+            newHead.prev = null;
+            j = 0;
+
+            if (ITERATIONS-1 == i) { j = (n-1-j); }
+
+            for(; j < n-1; j++) {
+                cursor = cursor.next;
+            }
+
+            if (ITERATIONS-1 != i) {
+                nextStart = cursor.next;
+            }
+
+            cursor.next = null;
+
+            endPoints = this._reverse(newHead);
+            start = endPoints[0];
+            end = endPoints[1];
+
+            if (i == 0) { prevTail = end; }
+            else { prevTail.next = start; prevTail = end }
+
+            if (i == 0) { this.head = start; }
+
+            cursor = nextStart;
+        }
+        debugger;
     }
 
     public size(): number {
@@ -103,11 +174,13 @@ export class List {
         this.length++;
     }
 
-    public reverseNth(n: number): void {
 
-    }
 
     public waterfall(): void {
 
+    }
+
+    public getHead(): ListNode {
+        return this.head;
     }
 }
